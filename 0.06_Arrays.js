66ada2c398025f0513734eeeaa09aef1d3bee1b9
Vernon Perry <pvernon30@gmail.com>
2017-08-17 13:16:31 -0400

    added arrays

---
+++ 0.06_Arrays.js
@@ -0,0 +1,17 @@
+//Arrays
+
+let students = ['Thomas', 'Madyson', 'Brandon', 'leon', 23, true, ['Jackson', 'Perry']];
+console.log(typeof(students))
+
+//grab the nested array out of the students array and grab 'perry'
+console.log('Hello', students[6][1], 'the platypus')
+let foodArr = ['quesadilla', 'pecan pie','potato', 'cheesecake', 'shrimp pasta']
+
+// for (let i in foodArr) {
+// 	console.log(foodArr[i])
+// }
+//---------------
+//SHORTHAND FOR FOR/IN LOOPS:
+foodArr.forEach(function(i) {
+	console.log(i)
+})
